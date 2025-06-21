ebd48a4450676f9fae4102f516339be4b41d29ec
NicCaterino <caterino.2054849@studenti.uniroma1.it>
2025-06-21 16:50:37 +0200

    risolto problema compilazione,pulizia cartelle e aggiornata visualizzazione RVIZ

--- run.sh
+++ run.sh
@@ -1,15 +1,24 @@
-
-
 #!/bin/bash
-
-# Simple launcher for multirobot simulator
 
 echo "Multirobot Simulator"
 echo "1) Build"
 echo "2) Run" 
 echo "3) Build and Run"
-read -p "Choose : " choice
+read -p "Choose [3]: " choice
 choice=${choice:-3}
+
+run_simulator() {
+    echo "Config: 1) Single robot  2) Multi robot"
+    read -p "Choose config [1]: " config
+    config=${config:-1}
+    cd catkin_ws
+    source devel/setup.bash
+    if [ "$config" = "2" ]; then
+        roslaunch multirobot_simulator simulator.launch config_file:=../test_data/multirobot_test.json
+    else
+        roslaunch multirobot_simulator simulator.launch
+    fi
+}
 
 case $choice in
     1)
@@ -21,29 +30,20 @@ case $choice in
         ;;
     2)
         echo "Running..."
-        echo "Config: 1) Single robot  2) Multi robot"
-        read -p "Choose config [1]: " config
-        config=${config:-1}
-        cd catkin_ws
-        source devel/setup.bash
-        if [ "$config" = "2" ]; then
-            roslaunch multirobot_simulator simulator.launch config_file:=/home/lattinone/Desktop/Multirobot-2d-simulator25/test_data/multirobot.json
-        else
-            roslaunch multirobot_simulator simulator.launch
-        fi
+        run_simulator
         ;;
     3)
         echo "Building..."
         cd catkin_ws
         catkin_make
-        source devel/setup.bash
-        echo "Build complete. Starting simulator..."
-        echo "Config: 1) Single robot  2) Multi robot"
-        read -p "Choose config [1]: " config
-        config=${config:-1}
-        if [ "$config" = "2" ]; then
-            roslaunch multirobot_simulator simulator.launch config_file:=/home/lattinone/Desktop/Multirobot-2d-simulator25/test_data/multirobot.json
-            roslaunch multirobot_simulator simulator.launch
+        if [ $? -eq 0 ]; then
+            source devel/setup.bash
+            echo "Build complete. Starting simulator..."
+            cd ..
+            run_simulator
+        else
+            echo "Build failed!"
+            exit 1
         fi
         ;;
     *)
